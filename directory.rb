0b2b5f10792580cf0adea42bce375b4168329037
Tom <tom.w.page@gmail.com>
2016-09-14 11:58:31 +0000

    second changes to list

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first we print the list of students
 puts "The students of Villains Academy"
 puts "--------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddie Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# finally, we print the total number of students
+print "Overall, we have"
+#its's important that we print() doesn't ass new line characters
+print 11
+puts " great students"
