8ee64fc2e2280d60e80774257853e1e22c7ff3a5
Tom <tom.w.page@gmail.com>
2016-09-14 17:08:53 +0000

    sixth commit. some hashes and arrays up in here!

--- directory.rb
+++ directory.rb
@@ -1,17 +1,17 @@
 
  students = [
 
- "Dr. Hannibal Lecter",
- "Darth Vader",
- "Nurse Ratched",
- "Michael Corleone",
- "Alex DeLarge",
- "The Wicked Witch of the West",
- "Terminator",
- "Freddie Krueger",
- "The Joker",
- "Joffrey Baratheon",
- "Norman Bates"
+ {name: "Dr. Hannibal Lecter", cohort: :november},
+ {name: "Darth Vader", cohort: :november},
+ {name: "Nurse Ratched", cohort: :november},
+ {name: "Michael Corleone", cohort: :november},
+ {name: "Alex DeLarge", cohort: :november},
+ {name: "The Wicked Witch of the West", cohort: :november},
+ {name: "Terminator", cohort: :november},
+ {name: "Freddie Krueger", cohort: :november},
+ {name: "The Joker", cohort: :november},
+ {name: "Joffrey Baratheon", cohort: :november},
+ {name: "Norman Bates", cohort: :november}
  ]
  
  def print_header
@@ -19,9 +19,9 @@
     puts "-----------------------"
  end
 
- def printr(names) 
-    names.each do |name|
-    puts name
+ def printr(students) 
+    students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
  end
 
@@ -31,7 +31,7 @@
 
  def print_footer(names)
 
-    print "Overall, we have, we have #{names.count} great students. "
+    print "Overall, we have, we have #{students.count} great students. "
     #its's important that we print() doesn't ass new line characters
  end
 
