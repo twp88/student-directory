b6f0891e3dc88c898dc81ac9e54e2bda98e2f4f0
Tom <tom.w.page@gmail.com>
2016-09-18 16:48:26 +0000

    Ex 11

--- directory.rb
+++ directory.rb
@@ -101,6 +101,7 @@ def print_menu
         puts "Choose an option"
         puts "1. Input student information"
         puts "2. Show the students"
+        puts "3. Save the list of students"
         puts "9. Exit the program"
 end
 
@@ -118,6 +119,8 @@ def process(selection)
            input_students
         when "2"
             show_students
+        when "3"
+            save_students
         when "9"
             exit
         else
@@ -133,5 +136,20 @@ def interactive_menu
     end
     
 end    
+
+def save_students
+    #open the file to which what we be going to write to
+    file = File.open("students.csv", "w")
+    #iterate students array
+    @students.each do |student|
+        student_data = [student[:name]], [student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line 
+    end 
+    file.close
+end 
+    
+        
+        
     
 interactive_menu
