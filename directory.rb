868dddf4354540d1a433a7f06800d6d15f6f98d4
Tom <tom.w.page@gmail.com>
2016-09-24 12:35:44 +0000

    Ex12 section 8

--- directory.rb
+++ directory.rb
@@ -1,176 +1,38 @@
-require 'csv'
-@students = []
-def add_to_students(name, cohort)
-    @students << {name: name, cohort: cohort.to_sym}
-    puts "Students has been successfully updated"
-end
-
 def input_students
-    
-    puts "Please enter the name of the students"
-    puts "To finish just hit return twice"
-    #create empty array
-    @students = []
-    #get the first name
-    name = STDIN.gets.chomp
-    
-    #make sure the user enters a name 
-    if name.empty? || name == '' || name == nil
-        puts "Please enter a name to start"
-        name = STDIN.gets.chomp
-    end
-    
-    
-    #chomp alternative
-    if name[-1] == ' '
-            name.chop!
-    end
-    
-    
-   
-        
-    #while the name is not empty, repeat this code
-    while !name.empty? do
-        puts "Which cohort are you studying in?"
-        cohort = STDIN.gets.chomp
-        if cohort.empty?
-            cohort = "november"
-        end
-        
-        #add the student hash to the array
-        add_to_students(name, cohort)#using method to add to students array
-        if @students.count == 1
-            puts "Now we have #{@students.count} student"
-        elsif
-            puts "Now we have #{@students.count} students"
-        end
-        
-        #get another name from the user
-        puts "Please enter the next students name, or press enter to leave."
-        name = STDIN.gets.chomp
-    end
-    
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
 end
 
-
-
- def print_header
-    puts "The students of Villians Academy".center(50)
-    puts "-----------------------".center(50)
- end
-
- def printr(students) 
-    @students.select do |student|  
-        
-        puts "#{student[:name]} (#{student[:cohort]} cohort)".center(50)
-        puts "Their hobby is #{student[:hobbies]}".center(50)
-        puts "They were born in #{student[:country_of_birth]}".center(50)
-        puts "They are #{student[:height]} tall".center(50)
-        
-    end
- end
-
-
-
-
-# finally, we print the total number of students
-
- def print_footer(names)
-    if names.count == 1
-        print "Overall, we have, we have #{names.count} great student. ".center(50)
-    elsif names.count > 1
-        print "Overall, we have, we have #{names.count} great students. ".center(50)
-    end
-    #its's important that we print() doesn't ass new line characters
- end
-
-def print_menu
-            #1. Print the options available
-        puts "Choose an option"
-        puts "1. Input student information"
-        puts "2. Show the students"
-        puts "3. Save the list of students to students.csv"
-        puts "4. Load students from students.csv"
-        puts "9. Exit the program"
+def print_header
+  puts 'The students of my cohort at Makers Academy'
+  puts "-------------"
 end
 
-
-def show_students
-        print_header
-        printr(@students)
-        print_footer(@students)
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 
-def process(selection)
-        
-        #3.do what the user wants
-       case selection
-       when "1"
-           input_students
-        when "2"
-            show_students
-        when "3"
-            save_students
-        when "4"
-            load_students
-        when "9"
-            exit
-        else
-            puts "What doo you mean?"
-        end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 
-def interactive_menu
-    try_load_students
-    loop do 
-        print_menu
-        process(STDIN.gets.chomp)
-    end
-    
-end    
+if students.length == 0
+    puts "There are no students entered in the student list"
+elsif students.length >= 1
+print_header
+print(students)
+print_footer(students)
 
-def save_students
-  # open the file for writing
-CSV.open("./students.csv", "wb") do |csv|
-    @students.each do |student|
-        csv << [student[:name], student[:cohort]]
-    end 
-end 
 end
-
-        
-        
-def load_students(filename = STDIN.gets.chomp)
-
-    CSV.foreach(filename) do |row|
-        line = row.inspect 
-        name, cohort = line.chomp.split(',')
-    
-        add_to_students(name, cohort)#using method to add to students array
-      end
-      puts "Students.csv has been successfully loaded"
-    
-end
-
-def try_load_students
-    puts "Please enter a name of a file that you wish to load"
-   filename = STDIN.gets.chomp #first arguement from the command line
-    if filename.nil?
-       filename = "students.csv"
-    end
-   
-   if File.exists?(filename)#if it exists
-       load_students(filename)
-       puts "Loaded #{@students.count} from #{filename}"
-    else #if it doesn't exist
-        puts "Sorry #{filename} doesn't exist"
-        exit
-    end
-    
-
-    
-end
-        
-    
-interactive_menu
